b4b63f1c4f16447e3282b88543ce33e6e9eb0b68
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-18 22:57:23 -0400

    creando TicketDto

---
+++ app/src/main/java/edu/ucne/registrootecnicos/data/remote/dto/TicketDto.kt
@@ -0,0 +1,11 @@
+package edu.ucne.registrootecnicos.data.remote.dto
+
+class TicketDto (
+    val fecha: String,
+    val prioridadId: Int,
+    val cliente: String,
+    val asunto: String,
+    val descripcion: String,
+    val tecnicoid: Int
+)
+
