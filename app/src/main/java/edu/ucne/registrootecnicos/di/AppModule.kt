b8b93baac3d48b2eb58b0d55a0a241dfca77525c
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-18 22:58:46 -0400

    Creando app module

---
+++ app/src/main/java/edu/ucne/registrootecnicos/di/AppModule.kt
@@ -0,0 +1,35 @@
+package edu.ucne.registrootecnicos.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registrootecnicos.data.remote.TecnicoApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    const val BASE_URL = "link-de-la-api"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideTicketingApi(moshi: Moshi): TecnicoApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(TecnicoApi::class.java)
+    }
+}
