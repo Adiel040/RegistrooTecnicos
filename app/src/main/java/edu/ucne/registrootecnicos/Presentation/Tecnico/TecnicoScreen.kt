69b9f1dc6166517ec76cf18d07c738ed026f1629
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-27 22:57:41 -0400

    corrigiendo algunos errores en tecnicoscreen

--- app/src/main/java/edu/ucne/registrootecnicos/Presentation/Tecnico/TecnicoScreen.kt
+++ app/src/main/java/edu/ucne/registrootecnicos/Presentation/Tecnico/TecnicoScreen.kt
@@ -29,29 +29,51 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import edu.ucne.registrootecnicos.data.local.database.TecnicoDb
 import edu.ucne.registrootecnicos.data.local.entities.TecnicoEntity
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TecnicoScreen(
     tecnicoId: Int,
     tecnicoDb: TecnicoDb,
-    onNavigateBack: () -> Unit
+    onNavigateBack: () -> Unit,
+    viewModel: TecnicoViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    TecnicoBodyScreen(
+        tecnicoId = tecnicoId,
+        tecnicoDb = tecnicoDb,
+        onNavigateBack = onNavigateBack,
+        onEvent = viewModel::onEvent,
+        uiState = uiState
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TecnicoBodyScreen(
+    tecnicoId: Int,
+    tecnicoDb: TecnicoDb,
+    onNavigateBack: () -> Unit,
+    onEvent: (TecnicoUiEvent) -> Unit,
+    uiState: TecnicoUiState
+){
     var nombre by remember { mutableStateOf("") }
     var sueldo by remember { mutableStateOf("") }
     var errorMessage: String? by remember { mutableStateOf(null) }
     val scope = rememberCoroutineScope()
 
-    LaunchedEffect(true) {
-        scope.launch{
-            val tecnico =  findTecnico(tecnicoDb,tecnicoId)
-            nombre = tecnico?.nombre ?: ""
-            sueldo = tecnico?.sueldo ?: ""
-        }
-    }
+//    LaunchedEffect(true) {
+//        scope.launch{
+//            val tecnico =  findTecnico(tecnicoDb,tecnicoId)
+//            nombre = tecnico?.nombre ?: ""
+//            sueldo = tecnico?.sueldo ?: ""
+//        }
+//    }
 
     Scaffold(
         topBar = {
@@ -78,20 +100,21 @@ fun TecnicoScreen(
             ) {
                 OutlinedTextField(
                     label = { Text(text = "Técnico") },
-                    value = nombre,
-                    onValueChange = { nombre = it },
+                    value = uiState.nombre,
+                    onValueChange = { onEvent(TecnicoUiEvent.NombreChanged(it)) },
                     modifier = Modifier.fillMaxWidth()
                 )
+
                 OutlinedTextField(
                     label = { Text(text = "Sueldo") },
-                    value = sueldo,
-                    onValueChange = { sueldo = it },
+                    value = uiState.sueldo,
+                    onValueChange = { onEvent(TecnicoUiEvent.SueldoChanged(it)) },
                     modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.padding(2.dp))
-                errorMessage?.let {
-                    Text(text = it, color = Color.Red)
-                }
+//                errorMessage?.let {
+//                    Text(text = it, color = Color.Red)
+//                }
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                 ) {
@@ -110,16 +133,9 @@ fun TecnicoScreen(
                     }
                     OutlinedButton(
                         onClick = {
-                            if (nombre.isBlank()) {
-                                errorMessage = "Nombre vacío"
-                            } else {
-                                scope.launch {
-                                    saveTecnico(tecnicoDb, TecnicoEntity(tecnicoid = if(tecnicoId == 0) null else tecnicoId, nombre = nombre, sueldo = sueldo))
-                                    nombre = ""
-                                    sueldo = ""
-                                    errorMessage = null
-                                    onNavigateBack()
-                                }
+                            scope.launch {
+                                onEvent(TecnicoUiEvent.PostTecnico)
+                                onNavigateBack()
                             }
                         }
                     ) {
