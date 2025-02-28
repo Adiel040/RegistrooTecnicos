0dc1a887b013c72fd4b7363bfc8b74824504bd32
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2025-02-27 23:00:48 -0400

    mejorando TecnicoRepository

--- app/src/main/java/edu/ucne/registrootecnicos/data/repository/TecnicoRepository.kt
+++ app/src/main/java/edu/ucne/registrootecnicos/data/repository/TecnicoRepository.kt
@@ -1,13 +1,29 @@
 package edu.ucne.registrootecnicos.data.repository
 
+import edu.ucne.registrootecnicos.data.remote.RemoteDataSource
+import edu.ucne.registrootecnicos.data.remote.Resource
 import edu.ucne.registrootecnicos.data.remote.TecnicoApi
 import edu.ucne.registrootecnicos.data.remote.dto.TecnicoDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class TecnicoRepository @Inject constructor(
-    private val api: TecnicoApi
+    private val remoteDataSource: RemoteDataSource
 ) {
-    suspend fun get() = api.get()
+    fun getAll(): Flow<Resource<List<TecnicoDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val clientes = remoteDataSource.getAll()
+            emit(Resource.Success(clientes))
+        } catch (e: HttpException) {
+            val errorMessage = e.response()?.errorBody()?.string() ?: e.message()
+            emit(Resource.Error("Error de conexion $errorMessage"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error ${e.message}"))
+        }
+    }
 
-    suspend fun post(tecnicoDto: TecnicoDto) = api.post(tecnicoDto)
+    suspend fun post(tecnicoDto: TecnicoDto) = remoteDataSource.post(tecnicoDto)
 }
